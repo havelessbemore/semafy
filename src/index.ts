acf39252cfa7c330828247e677ed8fa8b6fb8409
havelessbemore <dev.michael.rojas@gmail.com>
2024-05-08 20:55:48 -0400

    Add Semaphore class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { ConditionVariable } from "./conditionVariable";
 export { Mutex } from "./mutex";
+export { Semaphore } from "./semaphore";
 export { SharedMutex } from "./sharedMutex";
 export { TimeoutError } from "./utils/timeoutError";
---
+++ src/semaphore.ts
@@ -0,0 +1,156 @@
+import { ConditionVariable } from "./conditionVariable";
+import { Mutex } from "./mutex";
+import { TimeoutError } from "./utils/timeoutError";
+
+const ERR_NEG_COUNT = "Cannot release negative value.";
+const ERR_OVERFLOW = "Operation would cause overflow.";
+
+const MAX_VALUE = ~(1 << 31);
+
+/**
+ * A counting semaphore based on shared memory and atomics, allowing for
+ * cross-agent synchronization.
+ *
+ * @see {@link https://en.cppreference.com/w/cpp/thread/counting_semaphore | C++ std::counting_semaphore}
+ */
+export class Semaphore {
+  private _gate: ConditionVariable;
+  private _mem: Int32Array;
+  private _mutex: Mutex;
+
+  /**
+   * Creates a new instance of a Semaphore.
+   *
+   * @param sharedBuffer The shared buffer that backs the semaphore.
+   * @param byteOffset The byte offset within the shared buffer.
+   */
+  constructor(sharedBuffer: SharedArrayBuffer, byteOffset = 0) {
+    const bInt32 = Int32Array.BYTES_PER_ELEMENT;
+    this._mutex = new Mutex(sharedBuffer, byteOffset);
+    byteOffset += bInt32;
+    this._mem = new Int32Array(sharedBuffer, byteOffset, 1);
+    byteOffset += bInt32;
+    this._gate = new ConditionVariable(sharedBuffer, byteOffset);
+  }
+
+  /**
+   * Gets the underlying atomic handle.
+   */
+  get handle(): Int32Array {
+    return this._mem;
+  }
+
+  /**
+   * Acquires the semaphore, blocking until it is available.
+   *
+   * @returns A promise that resolves when acquisition is successful.
+   */
+  acquire(): Promise<void> {
+    // Acquire the internal mutex within scope
+    return this._mutex.request(async () => {
+      // Wait until internal counter has capacity
+      while (Atomics.load(this._mem, 0) <= 0) {
+        await this._gate.wait(this._mutex);
+      }
+
+      // Decrement internal counter
+      Atomics.sub(this._mem, 0, 1);
+    });
+  }
+
+  /**
+   * Attempts to acquire the semaphore.
+   *
+   * @returns A promise resolving to `true` if successful, otherwise `false`.
+   */
+  tryAcquire(): Promise<boolean> {
+    // Acquire the internal mutex within scope
+    return this._mutex.request(async () => {
+      // Check internal counter
+      if (Atomics.load(this._mem, 0) <= 0) {
+        return false;
+      }
+
+      // Decrement internal counter
+      Atomics.sub(this._mem, 0, 1);
+      return true;
+    });
+  }
+
+  /**
+   * Attempts to acquire the semaphore, blocking until either
+   * success or the specified timeout elapses.
+   *
+   * @param timeout The maximum duration in milliseconds to wait.
+   *
+   * @returns A promise resolving to `true` if successful, otherwise `false`.
+   */
+  tryAcquireFor(timeout: number): Promise<boolean> {
+    return this.tryAcquireUntil(performance.now() + timeout);
+  }
+
+  /**
+   * Attempts to acquire the lock, blocking until either
+   * the lock is acquired or the specified point in time is reached.
+   *
+   * @param timestamp The absolute time in milliseconds to wait until.
+   *
+   * @returns A promise resolved to `true` if succesful, otherwise `false`.
+   */
+  tryAcquireUntil(timestamp: number): Promise<boolean> {
+    // Acquire the internal mutex within scope
+    return this._mutex
+      .requestUntil(async () => {
+        // Wait until internal counter has capacity
+        while (Atomics.load(this._mem, 0) <= 0) {
+          await this._gate.waitUntil(this._mutex, timestamp);
+        }
+
+        // Decrement internal counter
+        Atomics.sub(this._mem, 0, 1);
+
+        // Return success
+        return true;
+      }, timestamp)
+      .catch((err) => {
+        // Ignore timeout errors
+        if (err instanceof TimeoutError) {
+          return false;
+        }
+
+        // Throw other errors
+        throw err;
+      });
+  }
+
+  /**
+   * Releases a specified number of units back to the semaphore.
+   *
+   * @param count The number of units to release. Defaults to 1.
+   *
+   * @throws {RangeError} If `count` is negative or would cause the semaphore to overflow.
+   */
+  release(count = 1): Promise<void> {
+    // Sanitize input
+    if (count < 0) {
+      throw new RangeError(ERR_NEG_COUNT);
+    }
+
+    // Acquire internal mutex within scope
+    return this._mutex.request(() => {
+      // Get the internal counter
+      const state = Atomics.load(this._mem, 0);
+
+      // Check for overflow
+      if (count > MAX_VALUE - state) {
+        throw new RangeError(ERR_OVERFLOW);
+      }
+
+      // Increment the internal counter
+      Atomics.add(this._mem, 0, count);
+
+      // Notify blocked agents
+      this._gate.notifyAll();
+    });
+  }
+}
