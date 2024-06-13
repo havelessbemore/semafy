8a0c3966a82027d8b24837ee8fbc2f944660d4cd
havelessbemore <dev.michael.rojas@gmail.com>
2024-06-12 20:34:18 -0400

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
-import eslint from "@eslint/js";
 import globals from "globals";
+
+import eslint from "@eslint/js";
 import prettierConfig from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigArray} */
 export default tseslint.config(
   {
-    ignores: ["dist/", "docs/"],
+    ignores: ["dist", "docs"],
     languageOptions: {
       globals: {
         ...globals.browser,
