e1787549c1ea64f33725913e2c69e00e917e2388
havelessbemore <dev.michael.rojas@gmail.com>
2024-06-12 20:34:55 -0400

    Update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,17 +2,12 @@ import { JestConfigWithTsJest } from "ts-jest";
 
 export default {
   preset: "ts-jest",
+  passWithNoTests: true,
   testEnvironment: "node",
   collectCoverageFrom: [
     "src/**/*.{js,jsx,ts,tsx}",
     "!**/node_modules/**",
     "!**/vendor/**",
   ],
-  coverageDirectory: "<rootDir>/coverage/",
-  coveragePathIgnorePatterns: [
-    "<rootDir>/benchmarks/",
-    "<rootDir>/examples/",
-    "<rootDir>/src/index.ts",
-    "<rootDir>/tests/",
-  ],
+  coveragePathIgnorePatterns: ["<rootDir>/src/index.ts"],
 } as JestConfigWithTsJest;
