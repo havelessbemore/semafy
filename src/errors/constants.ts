12b79f79e81f4c7a205252e0ef287a0eb4c61eff
havelessbemore <dev.michael.rojas@gmail.com>
2024-06-15 17:13:46 -0400

    Fix type in ERR_REC_MUTEX_OVERFLOW

--- src/errors/constants.ts
+++ src/errors/constants.ts
@@ -1,5 +1,5 @@
 // Generic
-export const ERR_TIMEOUT = "Operation timed out.";
+export const ERR_TIMEOUT = "Operation timed out";
 
 export const ERR_NEGATIVE_VALUE = "Value cannot be negative";
 
@@ -17,29 +17,29 @@ export const ERR_LATCH_INPUT_OVERFLOW =
 export const ERR_CV_VALUE = "Unexpected value in shared memory location";
 
 // Mutex
-export const ERR_LOCK = "A lock has encountered an error.";
+export const ERR_LOCK = "A lock has encountered an error";
 
 export const ERR_LOCK_OWNERSHIP =
-  "Operation not permitted. Lock must be acquired first.";
+  "Operation not permitted. Lock must be acquired first";
 
 export const ERR_LOCK_RELOCK =
-  "Attempted relock of already acquired lock. Deadlock would occur.";
+  "Attempted relock of already acquired lock. Deadlock would occur";
 
-export const ERR_LOCK_TIMEOUT = "Timed out acquiring lock.";
+export const ERR_LOCK_TIMEOUT = "Timed out acquiring lock";
 
 // Recursive Mutex
 
 export const ERR_REC_MUTEX_OVERFLOW =
-  "Operation not permitted. Additional lock would excee the RecursiveMutex's maximum levels of ownership.";
+  "Operation not permitted. Additional lock would exceed the maximum levels of ownership";
 
 // Mutex Management
-export const ERR_MULTI_LOCK = "Failed to acquire all locks.";
+export const ERR_MULTI_LOCK = "Failed to acquire all locks";
 
-export const ERR_MULTI_UNLOCK = "Failed to unlock all locks.";
+export const ERR_MULTI_UNLOCK = "Failed to unlock all locks";
 
 // Semaphore
 export const ERR_SEM_INPUT_NEG =
-  "Operation not permitted. Semaphore release value cannot be negative.";
+  "Operation not permitted. Semaphore release value cannot be negative";
 
 export const ERR_SEM_INPUT_OVERFLOW =
-  "Operation not permitted. Semaphore release would cause overflow.";
+  "Operation not permitted. Semaphore release would cause overflow";
